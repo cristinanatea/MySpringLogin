b6a5603a002ddffacdef8b5155255a8c2aca9c48
Cristina Natea <cristina.natea4@gmail.com>
2019-11-15 16:37:22 +0200

    MySpringLogin

---
+++ src/main/java/com/Spring/utilizator/dao/UtilizatorDAO.java
@@ -0,0 +1,12 @@
+package com.Spring.utilizator.dao;
+
+import com.Spring.utilizator.model.Utilizator;
+
+public interface UtilizatorDAO 
+{
+	public void insert(Utilizator utilizator);
+	public Utilizator findBUtilizatorId(int utilizatorID) ;
+		// TODO Auto-generated method stub
+		//return 0 ;
+	
+}
